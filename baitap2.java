ea6994bdcd4b6df3f779b963ec795fa889bec08b
Thái Thanh Nhật <97711427+thaithanhnhat@users.noreply.github.com>
2022-01-14 10:27:20 +0700

    Add files via upload

---
+++ baitap2.java
@@ -0,0 +1,8 @@
+// Khai báo class
+public class Baitap2 {
+// khai báo hàm main
+    public static void main(String[] args){
+
+        System.out.printf("Welcome to Java");// lệnh in chuổi trong ngoặc kép
+    }
+}
