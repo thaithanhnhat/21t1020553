4ff94d4d266172270d2c62fb1bc28eb241623b6e
Thái Thanh Nhật <97711427+thaithanhnhat@users.noreply.github.com>
2022-02-18 08:12:34 +0700

    Add files via upload

---
+++ Chuvi.java
@@ -0,0 +1,15 @@
+package BAITAPOJ;
+import java.util.Scanner;
+
+public class Chuvi {
+    public static void main(String[] args){
+        double n;
+        Scanner nhap =  new Scanner(System.in);
+        System.out.println("Nhap vao n: ");
+            n = nhap.nextDouble();         
+        System.out.print("Ket qua la: "+ 2*n*3.14159);
+        
+
+    } 
+    
+}
