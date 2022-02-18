1d16f4c1da97a97aef8e1b113449c58fb031c427
Thái Thanh Nhật <97711427+thaithanhnhat@users.noreply.github.com>
2022-02-18 08:21:23 +0700

    Add files via upload

--- Chuvi.java
+++ Chuvi.java
@@ -3,11 +3,20 @@ import java.util.Scanner;
 
 public class Chuvi {
     public static void main(String[] args){
-        double n;
-        Scanner nhap =  new Scanner(System.in);
-        System.out.println("Nhap vao n: ");
-            n = nhap.nextDouble();         
-        System.out.print("Ket qua la: "+ 2*n*3.14159);
+        boolean wenttocatch = false;
+        Scanner scan = new Scanner(System.in);
+        float number_of_rigons = 0;
+        do{
+            System.out.println("Nhap vao n : ");
+            if(scan.hasNextFloat()){
+                number_of_rigons = scan.nextFloat();
+                wenttocatch = true;
+            }else{
+                scan.nextLine();
+                System.out.println("Nhap lai n: ");
+            }
+        }while(!wenttocatch);
+        System.out.print("Ket qua la: "+ 2*number_of_rigons*3.14159);
         
 
     } 
